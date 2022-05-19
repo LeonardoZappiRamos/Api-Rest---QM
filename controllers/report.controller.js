fc723ca8412f64ae0fc1bdf5d3ed04d4f2ec24ab
LeonardoZappiRamos <leo.zappi@hotmail.com>
2022-05-18 22:53:41 -0300

    Reports

---
+++ controllers/report.controller.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+require('dotenv').config();
+
+const dirname = process.env.DIR_REPORTS.toString();
+
+const addReport = async (req, res) =>{
+
+  const report = {
+    title: req.body.title,
+    consult: dirname.toString()+'/'+req.body.title,
+    role: req.body.role,
+  }
+  res.status(201).json(report)
+}
+
+module.exports.addReport = addReport
+
+const listReports = async (req, res) => {
+  const role = req.body.Role;
+  console.log(role);
+  try {
+    const pasta = [];
+    fs.readdir(dirname, (err, dir) => {
+      dir.forEach(dirf => {
+          fs.readdir(dirname+'/'+dirf, (err, files) => {
+            if(dirf == role){
+              files.forEach(file => {
+                pasta.push(file);
+              })
+            res.status(200).json({
+              "Quantidade": pasta.length,
+              "Relatorios": pasta
+            });
+            }
+          })
+      })
+    })
+  }catch (err) {
+    res.status(500).send(err.stack);
+  }
+};
+
+module.exports.listReports = listReports
--- routes/report.js
+++ routes/report.js
@@ -2,11 +2,19 @@ const routes = require('express').Router();
 
 const verifyJWT = require('../middleware/verifyAuth')
 
+const { 
+  addReport,
+  listReports 
+} = require('../controllers/report.controller');
+
 routes.get('/', verifyJWT, (req, res) =>{
   res.send({
     "name": "Faturamento Diário",
     "Role": "Faturamento"
   })
 })
+
+routes.get('/list', listReports)
+routes.post('/register', addReport)
 
 module.exports = routes;
